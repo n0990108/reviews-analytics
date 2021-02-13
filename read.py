f2377376ef9389c4e5fc4da079779545e5f56adb
Nina Chen <n0990108@gmail.com>
2021-02-13 17:18:02 +0800

    add words_count

--- read.py
+++ read.py
@@ -8,6 +8,8 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了,總共有', len(data), '筆資料')
 
+print(data[0])
+
 sum_len = 0
 for d in data:
 	sum_len += len(d)	
@@ -20,3 +22,48 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100') 
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆資料包含"good"')
+print(good[0])
+
+good = [d for d in data if 'good' in d ]
+bad = ['bad' in d for d in data]
+
+
+# 文字計數
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] >= 500:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	elif word in wc:
+		print(word, '出現過的次數為', wc[word], '次')
+	else:
+		print('這個字沒有出現過哦!')
+print('感謝使用本查詢功能')
+	
+
+
+
+
+
+
+
+
